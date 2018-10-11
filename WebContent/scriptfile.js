898e166abc99cb7af24b8307f4d2678ae8249f41
Bishwo Shrestha <bishwo.shrestha2@gmail.com>
2018-10-11 17:26:01 -0400

    changes

--- WebContent/scriptfile.js
+++ WebContent/scriptfile.js
@@ -8,13 +8,37 @@
 
   /*To store the user data in localstorage */
   var storednames = JSON.parse(localStorage.getItem('Users'));
+  
+  var modalConfirm = function(callback){
+  
+  $("#btn-confirm").on("click", function(){
+    $("#mi-modal").modal('show');
+  });
+
+  $("#modal-btn-si").on("click", function(){
+    callback(true);
+    $("#mi-modal").modal('hide');
+  });
+  
+  $("#modal-btn-no").on("click", function(){
+    callback(false);
+    $("#mi-modal").modal('hide');
+  });
+};
+
+modalConfirm(function(confirm){
+  if(confirm){
+    //Acciones si el usuario confirma
+    $("#result").html("CONFIRMADO");
+  }else{
+    //Acciones si el usuario no confirma
+    $("#result").html("NO CONFIRMADO");
+  }
+});
 
 	
 	$(document).ready(function () 
-	{		
-	
-	//$('#finishbutton').hide();
-			
+	{				
 	
 	jQuery.validator.addMethod(
         'ContainsAtLeastTwoDigit',
@@ -25,6 +49,7 @@
     );  
 	
    $('form').each(function () {
+	   
         $(this).validate({
 	  	
 		errorContainer: $('#errorContainer'),
@@ -65,25 +90,67 @@
 				  require_from_group: [1,".accountdrop"]				  
 				},
 				
-				balance1: {
+				currentbalancedollar1: {
 				  currency: "$",
-				  require_from_group: [1,".balanceclass"]
+				  require_from_group: [1,".currentbalanceclass"]
 				},
-				balance1: {
+				currentbalancedollar2: {
 			      currency: "$",
-				  require_from_group: [1,".balanceclass"]				  
+				  require_from_group: [1,".currentbalanceclass"]				  
 				},
-				balance1: {
+				currentbalancedollar3: {
 					currency: "$",
-				  require_from_group: [1,".balanceclass"]				  
+				  require_from_group: [1,".currentbalanceclass"]				  
 				},
-				balance1: {
+				currentbalancedollar4: {
 					currency: "$",
-				  require_from_group: [1,".balanceclass"]				  
+				  require_from_group: [1,".currentbalanceclass"]				  
 				},
-				balance1: {
+				currentbalancedollar5: {
 					currency: "$",
-				  require_from_group: [1,".balanceclass"]				  
+				  require_from_group: [1,".currentbalanceclass"]				  
+				},
+				
+				currentbalancecents1: {
+				  currency: "$",
+				  require_from_group: [1,".currentbalanceclass2"]
+				},
+				currentbalancecents2: {
+			      currency: "$",
+				  require_from_group: [1,".currentbalanceclass2"]				  
+				},
+				currentbalancecents3: {
+					currency: "$",
+				  require_from_group: [1,".currentbalanceclass2"]				  
+				},
+				currentbalancecents4: {
+					currency: "$",
+				  require_from_group: [1,".currentbalanceclass2"]				  
+				},
+				currentbalancecents5: {
+					currency: "$",
+				  require_from_group: [1,".currentbalanceclass2"]				  
+				},
+				
+				expectbalance1: {
+				  currency: "$",
+				  require_from_group: [1,".expectbalanceclass"]
+				},
+				expectbalance2: {
+			      currency: "$",
+				  require_from_group: [1,".expectbalanceclass"]				  
+				},
+				expectbalance3: {
+					currency: "$",
+				  require_from_group: [1,".expectbalanceclass"]				  
+				},
+				expectbalance4: {
+					currency: "$",
+				  require_from_group: [1,".expectbalanceclass"]				  
+				},
+				expectbalance5: {
+					currency: "$",
+				  require_from_group: [1,".expectbalanceclass"]				  
 				},
 			
 			firstname:{
@@ -110,19 +177,19 @@
 				
 		messages: {
 					inst1:{
-						require_from_group:"Please type at least 1 institution name."
+						require_from_group:"Please enter at least 1 institution name."
 					},
 					inst2:{
-						require_from_group:"Please type at least 1 institution name."
+						require_from_group:"Please enter at least 1 institution name."
 					},
 					inst3:{
-						require_from_group:"Please type at least 1 institution name."
+						require_from_group:"Please enter at least 1 institution name."
 					},
 					inst4:{
-						require_from_group:"Please type at least 1 institution name."
+						require_from_group:"Please enter at least 1 institution name."
 					},
 					inst5:{
-						require_from_group:"Please type at least 1 institution name."
+						require_from_group:"Please enter at least 1 institution name."
 					},	
 
 					accounttype1:{
@@ -140,21 +207,54 @@
 					accounttype5:{
 						require_from_group:"Please select at least 1 account name."
 					},
+					
+					currentbalancedollar1:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancedollar2:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancedollar3:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancedollar4:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancedollar5:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					
+					currentbalancecents1:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancecents2:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancecents3:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancecents4:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
+					currentbalancecents5:{
+						require_from_group:"Please enter your current balance for the account in correct format."
+					},
 
-					balance1:{
-						require_from_group:"Please type your balance for the account."
+
+					expectbalance1:{
+						require_from_group:"Please enter your expected balance for the account."
 					},
-					balance2:{
-						require_from_group:"Please type your balance for the account."
+					expectbalance2:{
+						require_from_group:"Please enter your expected balance for the account."
 					},
-					balance3:{
-						require_from_group:"Please type your balance for the account."
+					expectbalance3:{
+						require_from_group:"Please enter your expected balance for the account."
 					},
-					balance4:{
-						require_from_group:"Please type your balance for the account."
+					expectbalance4:{
+						require_from_group:"Please enter your expected balance for the account."
 					},
-					balance5:{
-						require_from_group:"Please type your balance for the account."
+					expectbalance5:{
+						require_from_group:"Please enter your expected balance for the account."
 					},
 						   
 					firstname:{ 
@@ -181,9 +281,10 @@
 		groups: {
             inputGroup: "inst1 inst2 inst3 inst4 inst5",   
 			selectGroup: "accounttype1 accounttype2 accounttype3 accounttype4 accounttype5",
-			inputGroup2: "balance1 balance2 balance3 balance4 balance5",
-        },
-		
+			inputGroup2: "expectbalance1 expectbalance2 expectbalance3 expectbalance4 expectbalance5",
+			inputGroup3: "currentbalancedollar1 currentbalancedollar2 currentbalancedollar3 currentbalancedollar4 currentbalancedollar5",
+            inputGroup4: "currentbalancecents1 currentbalancecents2 currentbalancecents3 currentbalancecents4 currentbalancecents5",
+		},
 		/*
 		submitHandler: function(form) 
 		{
