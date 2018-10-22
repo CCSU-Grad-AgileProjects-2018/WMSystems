13b011b3573f48febc643ad9fc4bcdf7be8e6fc2
Bishwo Shrestha <bishwo.shrestha2@gmail.com>
2018-10-22 12:36:53 -0400

    changes

--- WebContent/scriptfile.js
+++ WebContent/scriptfile.js
@@ -19,10 +19,37 @@
 	
 	$(document).ready(function () 
 	{	
+	
+	 $(function() {
+    $('.date-picker').datepicker( {
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+        dateFormat: 'MM yy',
+        onClose: function(dateText, inst) { 
+            
+            
+            
+            function isDonePressed(){
+                            return ($('#ui-datepicker-div').html().indexOf('ui-datepicker-close ui-state-default ui-priority-primary ui-corner-all ui-state-hover') > -1);
+                        }
 
-	var num = $("#datevalue").val();
-     alert(num);
-   
+                        if (isDonePressed()){
+
+                            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+                            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+                            $(this).datepicker('setDate', new Date(year, month, 1));
+                             console.log('Done is pressed')
+
+                        }
+            
+            
+          
+        }
+    });
+});
+
+	
 		
 	$('#ebalance1').keyup(function() {			
     if($(this).val() == '')
